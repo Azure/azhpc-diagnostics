857f7c6e114aa87a6544aa4d3fa8eb8e3515ba0a
Tigar Cyr <tigarcyr@microsoft.com>
2021-10-29 12:56:41 -0700

    increment version

--- Linux/src/gather_azhpc_vm_diagnostics.sh
+++ Linux/src/gather_azhpc_vm_diagnostics.sh
@@ -70,7 +70,7 @@ GPU_PCI_CLASS_ID=0302
 declare -A CPU_LIST
 CPU_LIST=(["Standard_HB120rs_v2"]="0 1,5,9,13,17,21,25,29,33,37,41,45,49,53,57,61,65,69,73,77,81,85,89,93,97,101,105,109,113,117"
           ["Standard_HB60rs"]="0 1,5,9,13,17,21,25,29,33,37,41,45,49,53,57")
-RELEASE_DATE=20211028 # update upon each release
+RELEASE_DATE=20211029 # update upon each release
 COMMIT_HASH=$( 
     (
         command -v git >/dev/null &&
